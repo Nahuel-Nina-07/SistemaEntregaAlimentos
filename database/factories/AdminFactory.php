ba083a6354fdde25919ea6e157b6f521b6e5783d
Nahuel <cristian.nina@uab.edu.bo>
2023-09-29 01:34:17 +0200

    Corrigiendo error en password AdminFactory

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -21,7 +21,7 @@ class AdminFactory extends Factory
             'name' => 'Admin1',
             'email' => 'admin1@gmail.com',
             'email_verified_at' => now(),
-            'password' => '12345678', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'remember_token' => Str::random(10),
