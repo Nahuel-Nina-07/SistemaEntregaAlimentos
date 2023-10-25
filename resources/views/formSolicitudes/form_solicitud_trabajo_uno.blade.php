7106bc3a46ed120e3631394fe6138663bda4b241
Cristian Martinez <donato.martinez@uab.edu.bo>
2023-10-25 23:00:04 +0200

    Subiendo vista formulario trabajo

--- resources/views/formSolicitudes/form_solicitud_trabajo_uno.blade.php
+++ resources/views/formSolicitudes/form_solicitud_trabajo_uno.blade.php
@@ -1,20 +1,130 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/Repartidor/form.css') }}">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registro Trabajo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <style>
+        html,body{
+            margin:0;
+            padding:0;
+        }
+        .navbar {
+            background-color:#FF416C;
+            color: #f2f2f2;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px 20px;
+        }
+
+        .navbar-brand {
+            color:#f2f2f2;
+            font-size: 24px;
+            margin: 0;
+        }
+
+        .dropdown {
+            position: relative;
+            display: inline-block;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #FF416C;
+            min-width: 150px;
+            z-index: 1;
+            right: 0;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+
+        .dropdown-item {
+            padding: 10px;
+            text-decoration: none;
+            display: block;
+            color: #f2f2f2;
+        }
+
+        .dropdown-item:hover {
+            background-color: #f2f2f2;
+            color:#FF416C;
+            
+        }
+        .imagen1 img{
+          display:flex;
+          justify-content:center;
+          align-items:center;
+          width:100%;
+          height:600px;
+        }
+        .btn{
+          background-color:#FF416C;
+          color:#f2f2f2;
+        }
+        .btn:hover{
+          background-color:#E0E0E0;
+          color:#FF416C;
+        }
+        .contenedor-form{
+            width:30%;
+            padding:1%;
+            background-color:#f2f2f2;
+            border-radius:15px;
+            margin-right:5%;
+            margin-top:-35%;
+            margin-bottom:5%;
+        }
+        .alinear-derecha{
+            width:100%;
+            display:flex;
+            justify-content:end;
+        }
+        .boton-subir{
+            margin-top:3%;
+        }
+        footer{
+            text-align:center;
+            background-color:#FF416C;
+            color: #f2f2f2;
+            height:auto;
+        }
+        .text-footer{
+            margin:0;
+        }
+    </style>
 </head>
 
 <body>
-    <!-- partial:index.partial.html -->
-    <div class="demo-page">
+    <div class="navbar">
+        <div class="navbar-brand">AlmanacProject</div>
+        <div class="dropdown">
+            <a class="dropdown-item" href="#">Inicio</a>
+            <div class="dropdown-content">
+                @auth
+                <a class="dropdown-item" href="{{ url('/dashboard') }}">Dashboard</a>
+                @else
+                <a class="dropdown-item" href="{{ route('login') }}">Login</a>
+                <a class="dropdown-item" href="{{ route('repartidor.create') }}">Solicitar Trabajo</a>
+                <a class="dropdown-item" href="{{ route('welcome') }}">Home</a>
+                @endauth
+            </div>
+        </div>
+    </div>
+    <div class="imagen1">
+        <img src="https://static.vecteezy.com/system/resources/previews/001/900/914/non_2x/blurred-restaurant-scene-with-empty-tabletop-free-photo.jpg" alt="imagen inicial">
+    </div>
+    <div class="alinear-derecha">
+        <div class="contenedor-form">
         <!-- <div class="demo-page-navigation"></div> -->
 
-        <main class="demo-page-content">
-            <section>
+        
                 <!-- <div class="href-target" id="input-types"></div> -->
                 <h1>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-align-justify">
@@ -25,21 +135,21 @@
                     </svg>
                     Ingresa tus datos
                 </h1>
-                <p>Por normas de seguridad necesitamos que lenes todos los campos.</p>
+                <p>Por normas de seguridad necesitamos que llenes todos los campos.</p>
                 <form method="POST" action="{{ url('/guardar-basicos') }}" enctype="multipart/form-data">
                     @csrf
                     <div class="nice-form-group">
-                        <label>Nombre</label>
-                        <input type="text" name="nombre_solicitante" placeholder="Ingrese su nombre" value="" required />
+                        <label class="form-label">Nombre</label>
+                        <input class="form-control" type="text" name="nombre_solicitante" placeholder="Ingrese su nombre" value="" required />
                     </div>
 
                     <div class="nice-form-group">
-                        <label>Apellido</label>
-                        <input type="text" name="apellido_solicitante" placeholder="Ingrese su apellido" value="" required />
+                        <label class="form-label">Apellido</label>
+                        <input class="form-control" type="text" name="apellido_solicitante" placeholder="Ingrese su apellido" value="" required />
                     </div>
 
                     <fieldset class="nice-form-group">
-                        <label>¿Tienes más de 18 años?</label>
+                        <label class="form-label">¿Tienes más de 18 años?</label>
                         <div class="nice-form-group">
                             <input type="radio" name="edad" id="r-1" value="1" />
                             <label for="r-1">Si</label>
@@ -52,26 +162,14 @@
                     </fieldset>
 
                     <div class="nice-form-group">
-                        <label>Numero de cédula o pasaporte</label>
-                        <input type="tel" placeholder="Ingrese su cédula/pasaporte(sin guion)" value="" name="ci_numero" required />
+                        <label class="form-label">Numero de cédula o pasaporte</label>
+                        <input class="form-control" type="tel" placeholder="Ingrese su cédula/pasaporte(sin guion)" value="" name="ci_numero" required />
                     </div>
-
-                    <br>
-                    <details>
-                        <summary>
-                            <button type="submit" onclick="mostrarMensajeRequisito()">
-                                <div class="toggle-code">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-code">
-                                        <polyline points="16 18 22 12 16 6" />
-                                        <polyline points="8 6 2 12 8 18" />
-                                    </svg>
-                                    Enviar
-                                </div>
-                            </button>
-                        </summary>
-                    </details>
-                    <br>
-                    <br>
+                    <div class="boton-subir">
+                        <button class="btn" type="submit" onclick="mostrarMensajeRequisito()">
+                            Enviar
+                        </button>
+                    </div>
                     @if ($errors->any())
                     <div class="alert alert-danger">
                         <ul>
@@ -83,11 +181,11 @@
                     @endif
 
                 </form>
-            </section>
-
-        </main>
-
+        </div>
     </div>
+    <footer>
+        <p class="text-footer">Derechos de autor © 2023 AlmanacProject. Todos los derechos reservados.</p>
+  </footer>
 </body>
 
 </html>
